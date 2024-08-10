5371e821cc1e4f8c52eb4e39926946ae854ffb67
Ricardo Teixeira <ricardo85x@gmail.com>
2024-08-10 01:38:28 -0300

    feat: new springboot project

---
+++ src/main/java/com/ricardoft/amqp/tutorials/rabbitmq_amqp_tutorials/RabbitmqAmqpTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.ricardoft.amqp.tutorials.rabbitmq_amqp_tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqAmqpTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqAmqpTutorialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardoft/amqp/tutorials/rabbitmq_amqp_tutorials/RabbitmqAmqpTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardoft.amqp.tutorials.rabbitmq_amqp_tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqAmqpTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
